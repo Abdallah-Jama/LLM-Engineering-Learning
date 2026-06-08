42e2bb74ba9068b7915c6d75a16be573b9a45e5e
Abdallah Jama <abdellah4jama@gmail.com>
2026-06-08 23:53:53 +0400

    Week 1 Day 1 — initial commit

---
+++ day1.py
@@ -0,0 +1,11 @@
+# imports 
+
+import os
+from dotenv import load_dotenv
+from scraper import fetch_website_contents
+from IPython.display import Markdown, display
+from openai import OpenAI
+
+
+
+
